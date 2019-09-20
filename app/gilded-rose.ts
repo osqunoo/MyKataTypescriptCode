58df7bff3342c4390f6ba6ab6318077669994e76
osama qunoo <osama.qunoo@realsoft-me.com>
2019-09-21 00:05:49 +0300

    Refactoring the assignment for increase and decrease

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -15,7 +15,7 @@ let updateItems = function(item) {
     if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (item.quality > MIN_LIMIT) {
             if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality = item.quality - 1;
+                item.quality -= 1;
             }
         }
     }
@@ -25,36 +25,36 @@ let updateItems = function(item) {
             if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
                 if (item.sellIn < 11) {
                     if (item.quality < MAX_LIMIT) {
-                        item.quality = item.quality + 1;
+                        item.quality += 1;
                     }
                 }
                 if (item.sellIn < 6) {
                     if (item.quality < MAX_LIMIT) {
-                        item.quality = item.quality + 1;
+                        item.quality += 1;
                     }
                 }
             }
         }
     }
     if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.sellIn = item.sellIn - 1;
+        item.sellIn -= 1;
     }
     if (item.sellIn < 0) {
         if (item.name != 'Aged Brie') {
             if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (item.quality > MIN_LIMIT) {
                     if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                        item.quality = item.quality - 1;
+                        item.quality -= 1;
                     }
                 }
             }
             else {
-                item.quality = item.quality - item.quality;
+                item.quality -= item.quality;
             }
         }
         else {
             if (item.quality < MAX_LIMIT) {
-                item.quality = item.quality + 1;
+                item.quality += 1;
             }
         }
     }
